9dc42db8ac08ed0bd9eac90681488934d7fd894a
Yekhantmaung <yekhantmaung0@gmail.com>
2025-12-21 00:53:18 +0630

    Add files via upload

--- Array.java
+++ Array.java
@@ -2,26 +2,25 @@ import java.util.Scanner;
 public class Array {
     public static void main(String []args){
         Scanner sca = new Scanner(System.in);
+        
         System.out.print("Enter size of array : ");
         int size = sca.nextInt();
-        
-        int[] arrayValue = new int[size];
-        int sum = 0;
-        
-        for (int a = 0; a < size; a++) {
-            while (true) {
-                System.out.printf("Enter value array index of %d : ",a);
-                int value = sca.nextInt();
-            if (value >= 0 && value <= 50) {
-                arrayValue[a] = value;
-                sum += value; // sum = sum + value
-                break;
-            }else {
-                System.out.println("Try Again!.");
-            }
-            }
+        int array[] = new int[size];
+
+        for (int a = 0;a < size;a++) {
+            System.out.print("Enter number : ");
+            array[a] = sca.nextInt();
         }
-        System.out.println("The sum result is : "+sum);
+
+        System.out.println("Even Number.");
+        for (int num : array) {
+            if (num % 2 == 0) {System.out.println(num + "");}
+        }
+        
+        System.out.println("Odd Number.");
+        for (int num : array) {
+            if (num % 2 != 0) {System.out.println(num + "");}
+        }
         sca.close();
     }
 }
